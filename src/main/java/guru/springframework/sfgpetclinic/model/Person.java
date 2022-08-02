02051a87ad45cb158f8e83839b22cfcabdf7aac8
Lucas Costa <lnatanfrancacosta@gmail.com>
2022-08-01 21:23:56 -0300

    add pojo classes, closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+}
